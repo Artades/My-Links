09c21022adef487bb8b809d3e521cfdaf61dc193
Artyom Galay <86312709+Artades@users.noreply.github.com>
2023-04-16 11:21:33 +0600

    Update about.ts

--- pages/api/about.ts
+++ pages/api/about.ts
@@ -5,7 +5,7 @@ const about = [
 		name: "Ivgam's Links",
 		avatarUrl: "/images/about/avatar.jpg",
 		email:
-			"https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvmZlJvjxwhlzdcmGtNjBncJQVqcsJgMPMCHXfxJKkqnDDXRJpgxtvxLlgfxZpLwwjZsJB",
+			"mailto:artemijgalaj@gmail.com",
 		description:
 			"Hi there!👋 My name's Artyom, i'm a web developer who thrives on raising the bar for what's possible with cutting-edge technologies. I've been on this journey since the age of 16, and I've spent countless hours honing my skills in HTML, CSS, and JS. Along the way, I've also immersed myself in Node.js, database design, and other exciting tools and technologies. Today, I have great expirience in JavaScript, TypeScript, React, and React Native, and I'm always looking for ways to deliver engaging, innovative web experiences. Take a look at my apps to see some of my most impressive projects, as possible you can support me by links below and let's raise the bar together, my freind!😇 🚀 ",
 	},
