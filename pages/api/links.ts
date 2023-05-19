43b5f60fe3f374c0f5ed077ca1a26f8c34ce0cd9
Artyom Galay <86312709+Artades@users.noreply.github.com>
2023-05-19 23:58:55 +0600

    Update links.ts

--- pages/api/links.ts
+++ pages/api/links.ts
@@ -139,7 +139,7 @@ const links = [
 		id: 11,
 		title: "Softlix: Cinema App",
 		path: "https://softlix-gules.vercel.app",
-		imageUrl: "/images/links/icons8-popcorn (1).png",
+		imageUrl: "/images/links/icons8-popcorn-96 (1).png",
 		gradient: {
 			from: "from-zinc-800",
 			to: "to-rose-600",
