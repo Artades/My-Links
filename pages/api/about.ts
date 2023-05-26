31c6499577b82d66f36193a615682b1735049766
Artyom Galay <86312709+Artades@users.noreply.github.com>
2023-05-26 17:31:11 +0600

    Update about.ts

--- pages/api/about.ts
+++ pages/api/about.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 const about = [
 	{
 		name: "Artyom's Links",
-		avatarUrl: "/images/about/IMG_20230526_172754_371.jpg",
+		avatarUrl: "/images/about/IMG_20230526_172952_897.jpg",
 		email:
 			"mailto:artemijgalaj@gmail.com",
 		description:
