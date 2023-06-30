84bf93e9952961b5dd0e98d99104a8b32407710a
Artyom Galay <86312709+Artades@users.noreply.github.com>
2023-06-30 23:33:25 +0600

    Update links.ts

--- pages/api/links.ts
+++ pages/api/links.ts
@@ -7,7 +7,7 @@ const links = [
 		path: "https://fluux.vercel.app",
 		imageUrl: "/images/links/flux.png",
 		gradient: {
-			from: "from-white",
+			from: "from-slate-300",
 			to: "to-fuchsia-500",
 		},
 		recent: {
